d927c0668fe44737285596b38a60d0e3eff4f31d
kyrexa <arzamas.orko@yandex.ru>
2022-10-20 19:37:41 +0300

    Сделал домашнее задание 20.10.22

---
+++ HomeWork_41/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+Console.Write("Введите M - кол-во символов : ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for(int i = 1; i <= m; i++)
+{
+    Console.Write($"Введите число №{i}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if(number > 0) count++;
+}
+Console.WriteLine($"Чисел больше 0: {count}");
---
+++ HomeWork_43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y= k1*x+b1, y= k2*x+b2; значения b1, k1, b2, и k2 задаются пользователем.
+Console.Clear();
+Console.Write("введите значение b1 ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число k1 ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите значение b2 ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число k2 ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double a = (-b2 + b1)/(-k1 + k2);
+double b = k2 * a + b2;
+
+Console.WriteLine($"Точки пересечения прямых А: {a}, B: {b}");
